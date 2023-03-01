86186279d48fbd8e93c44f281202efaef476929e
Sanprieto <ricardosanprieto@gmail.com>
2023-03-01 19:19:37 +0100

    Componente para producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   imgParent = '';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Automobil de juguete',
+      price: 100,
+      image: './assets/images/1.jpg'
+  },
+  {
+      id: '2',
+      name: 'Muñeca de trapo',
+      price: 180,
+      image: './assets/images/2.jpg'
+  },
+  {
+      id: '3',
+      name: 'Pelota de futbol',
+      price: 120,
+      image: './assets/images/3.jpg'
+  }
+
+  ]
 
   onLoaded( img:string ){
     console.log('padre', img)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
 import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent
+    ImgComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
